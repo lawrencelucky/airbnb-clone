260564e8db7c427a843816aae4614ae576c05bb4
lawrencelucky <lawrencelucky1999@gmail.com>
2020-11-24 17:13:18 +0100

    Feat: Adding the footer

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>&copy; 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+      <p>Privacy &bull; Terms &bull; Sitemap &bull; Company Details</p>
+    </div>
+  );
+};
+
+export default Footer;
